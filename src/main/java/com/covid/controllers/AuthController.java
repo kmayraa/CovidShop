ef1d648c140ad86cf867f681f54ed7420d0615bb
kmayraa <kmar.tizaoui@gmail.com>
2021-04-05 23:17:46 +0200

    second commit

--- src/main/java/com/covid/controllers/AuthController.java
+++ src/main/java/com/covid/controllers/AuthController.java
@@ -32,7 +32,7 @@ import com.covid.repository.UserRepository;
 import com.covid.security.jwt.JwtUtils;
 import com.covid.security.services.UserDetailsImpl;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
